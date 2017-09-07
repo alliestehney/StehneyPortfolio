22ba05e7e3f0601a4fd788e8f9c0fd7e5ced6686
alliestehney <alliestehney1@gmail.com>
2017-09-07 13:37:22 -0400

    Added javascript function to change the size of 'p.s' font on about page

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,8 @@
 function showInput() {
 	alert("Thanks, "+ document.getElementById("nameinput").value + ", for your submission! I'll get back to you as soon as I can.")
 }
+
+function makeBigger() {
+	var ps = document.getElementById('ps');
+	ps.style.fontSize = "32px";
+}
