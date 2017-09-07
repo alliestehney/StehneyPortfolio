b8660cd2d882fd4590be775136508750159f9a58
alliestehney <alliestehney1@gmail.com>
2017-09-07 13:52:54 -0400

    Added form validation and javascript submit function

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,27 @@
-function showInput() {
-	alert("Thanks, "+ document.getElementById("nameinput").value + ", for your submission! I'll get back to you as soon as I can.")
+// document.getElementById("contact").submit();
+
+// function showInput() {
+// 	alert("Thanks, "+ document.getElementById("nameinput").value + ", for your submission! I'll get back to you as soon as I can.")
+// }
+
+function submit_by_id() {
+	var name = document.getElementById("nameinput").value;
+	var email = document.getElementById("emailinput").value;
+	if (formValidation()) {
+		document.getElementById("contact").submit();
+	}
+}
+
+function formValidation() {
+	var name = document.getElementById("nameinput").value;
+	var email = document.getElementById("emailinput").value;
+	var message = document.getElementById("messageinput").value;
+	if(name === '' || email === '' || message === '') {
+		alert("Please fill out every field!");
+		return false;
+	}else {
+		return true;
+	}
 }
 
 function makeBigger() {
