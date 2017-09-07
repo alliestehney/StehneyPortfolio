4e8cd513b5e484e0b473eae6f58dc102083f21d8
alliestehney <alliestehney1@gmail.com>
2017-09-07 14:01:05 -0400

    Cleaned up code and renamed a function

--- portfolio.js
+++ portfolio.js
@@ -2,7 +2,7 @@ function showInput() {
 	alert("Thanks, "+ document.getElementById("nameinput").value + ", for your submission! I'll get back to you as soon as I can.")
 }
 
-function submit_by_id() {
+function submit_form_by_id() {
 	var name = document.getElementById("nameinput").value;
 	var email = document.getElementById("emailinput").value;
 	if (formValidation()) {
